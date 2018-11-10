9705a37253713fa9be1132cd0b4187ce77fc0549
mwkrier <michaelwkrier@gmail.com>
2018-11-10 11:47:37 -0500

    build deploy

--- build/script1.js
+++ build/script1.js
@@ -1,4 +1,4 @@
 $('document').ready(function() {
-    var options = { videoId: 'ab0TSkLe-E0', start: 3 };
+    var options = { videoId: '0YjLf8T0lUs', start: 1 };
     $('#wrapper').tubular(options);
     });
